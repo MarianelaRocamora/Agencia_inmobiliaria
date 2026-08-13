76dd142f94b222aca47f51260a670a21abb1db3b
Marianela <rocamoramarianela86@gmail.com>
2026-08-13 16:20:49 -0300

    commit .net

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Agencia_inmobiliaria.Models;
+
+namespace Agencia_inmobiliaria.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
